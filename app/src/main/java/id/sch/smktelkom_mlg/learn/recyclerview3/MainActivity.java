ab36be75b9cb82092f58816ce57d9e9533f00593
sigitapriyan <dengansigit@gmail.com>
2016-11-06 09:34:50 +0700

    RecyclerView List Card

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
@@ -1,15 +1,31 @@
 package id.sch.smktelkom_mlg.learn.recyclerview3;
 
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
+import model.Hotel;
+import adapter.HotelAdapter;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Hotel> mList = new ArrayList<>();
+    HotelAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +35,49 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new HotelAdapter(mList);
+        recyclerView.setAdapter(mAdapter);
+
+        fillData();
+    }
+
+    private void fillData()
+    {
+        Resources resources = getResources();
+        String [] arJudul = resources.getStringArray(R.array.places);
+        String [] arDeskripsi = resources.getStringArray(R.array.place_desc);
+        TypedArray a = resources.obtainTypedArray(R.array.places_picture);
+        Drawable[] arFoto = new Drawable[a.length()];
+
+        for (int i = 0; i < arFoto.length; i++)
+        {
+            BitmapDrawable bd =(BitmapDrawable) a.getDrawable(i);
+            RoundedBitmapDrawable rbd =
+                    RoundedBitmapDrawableFactory.create(getResources(),bd.getBitmap());
+            rbd.setCircular(false);
+            arFoto [i] = rbd;
+        }
+
+        a.recycle();
+
+        for (int i = 0;i < arJudul.length; i++)
+        {
+            mList.add(new Hotel(arJudul[i],arDeskripsi[i],arFoto[i]));
+        }
+        mAdapter.notifyDataSetChanged();
     }
 
     @Override
